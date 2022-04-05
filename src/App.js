9409463733de9fd9cf4b1538bb9d3fca2bf8dc0e
Fatima Mohamedali <fatimamohamedali@fatimas-mbp.myfiosgateway.com>
2022-04-05 15:42:20 -0400

    add and delete tasks from server

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
 import React from "react";
 import Header from "./Components/Header";
 import Tasks from "./Components/Tasks";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddTask from "./Components/AddTask";
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 2,
-      text: "Cooking for Ramadan",
-      day: "Feb 8th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: 1,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks();
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
 
   // Delete a Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    // Delete Request
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   //   Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
 
-    setTasks([...tasks, newTask]);
+    const data = res.json()
+
+    setTasks([...tasks, data])
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+
+    // setTasks([...tasks, newTask]);
   };
 
   //   Toggle Reminder
